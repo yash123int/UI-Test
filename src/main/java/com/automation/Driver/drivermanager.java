91e16a19cee80a10dae0dcc87bfaff9a47718d08
IN\Yashkumar.Singh <Yashkumar.Singh@ril.com>
2026-05-05 18:24:48 +0530

    Added headless support in drivermanager for pipeline execution

--- src/main/java/com/automation/Driver/drivermanager.java
+++ src/main/java/com/automation/Driver/drivermanager.java
@@ -2,15 +2,20 @@ package com.automation.Driver;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class drivermanager {
     private static WebDriver driver;
 
     public static WebDriver init(){
-        if(driver == null){
-            driver = new ChromeDriver();   
-            driver.manage().window().maximize(); 
-        }
+       ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless=new");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--window-size=1920,1080");
+        options.addArguments("--disable-gpu");
+        
+        driver = new ChromeDriver(options);
         return driver;
     }
 
